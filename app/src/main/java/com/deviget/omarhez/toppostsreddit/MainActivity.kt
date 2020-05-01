0021f57b1745ac6f6d0c3b5dab6df4f83d1e0ae4
Omar Hezi <omar.hezi@mediamonks.com>
2020-04-30 23:24:17 -0300

    initial commit

---
+++ app/src/main/java/com/deviget/omarhez/toppostsreddit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.deviget.omarhez.toppostsreddit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
